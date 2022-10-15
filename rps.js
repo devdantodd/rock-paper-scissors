737c55d1c445c0be3778878675eeb293e35ab076
Dan Todd <devdantodd@proton.me>
2022-10-15 13:39:29 -0600

    uploading base files

---
+++ rps.js
@@ -0,0 +1,11 @@
+
+     function selections(playerSelection, computerSelection) {
+        var playerSelection = ["Rock", "Paper", "Scissors"];
+        var computerSelection = "getComputerChoice()";
+        var choice = options[Math.floor(Math.random()*options.length)];   
+        console.log(choose(playerSelection, computerSelection));
+     };
+
+    
+        
+    console.log(playerSelection);
