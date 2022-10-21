087bd1901e264071baf9eb4898dc49e5a8cc4f69
Dan Todd <devdantodd@proton.me>
2022-10-21 16:31:06 -0400

    4:32pm 10/21/22

--- rps.js
+++ rps.js
@@ -1,11 +1,36 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+   {
+      name: 'rock',
+      emoji: '✊',
+      beats: 'scissors'
+   },
+   {
+      name: 'paper',
+      emoji: '✋',
+      beats: 'rock'
+   },
+   {
+      name: 'scissors',
+      emoji: '✌️',
+      beats: 'paper'
+   }
+]
 
-     function selections(playerSelection, computerSelection) {
-        var playerSelection = ["Rock", "Paper", "Scissors"];
-        var computerSelection = "getComputerChoice()";
-        var choice = options[Math.floor(Math.random()*options.length)];   
-        console.log(choose(playerSelection, computerSelection));
-     };
+selectionButtons.forEach(selectionButton => {
+   selectionButton.addEventListener('click', e => {
+      const selectionName = selectionButton.dataset.selection
+      const selection = SELECTIONS.find(selection => selection.name === selectionName)
+      makeSelection(selection)
+   })
+})
 
-    
-        
-    console.log(playerSelection);
+function makeSelection(selection) {
+   const computerSelection = randomSelection()
+   console.log(selection)
+}
+
+function randomSelection() {
+   const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+   return SELECTIONS[randomIndex]
+}
